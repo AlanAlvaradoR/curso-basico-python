66971524d64ca7910d6e433d7b848ab173c17b40
AlanAlvaradoR <ceinnova10931@innovaccion.mx>
2022-05-30 23:33:20 -0500

    Problemas

---
+++ arreglos/funciones-arreglos.py
@@ -0,0 +1,36 @@
+#Arreglo o lista
+lista = [1, 2, 3, 4, 5]
+
+lista.append(0) #Poner un elemento al final de la lista
+print(lista)
+
+lista.extend(lista) #Agregar una lista (o un iterable) a la lista
+print(lista)
+
+lista = lista[::-1] #Invertir una lista (se usa más para iterables)
+print(lista)
+
+lista.reverse() #Invertir una lista
+print(lista)
+
+lista.insert(0, 10) #Insertar un elemento en una posición especifica (posicion, valor)
+lista.insert(5,11)
+print(lista)
+
+lista.remove(0) #Elimina un elemento de la lista (busca el primer elemento con el valor dado y lo elimina)
+lista.remove(0)
+print(lista)
+
+print(lista.pop(1)) #Igual que .remove pero este lo devuelve
+print(lista)
+
+print(lista.count(3)) #Cuenta la cantidad de veces que aparece un elemento en la lista
+
+print(lista.index(10)) #Devuelve la posición de un elemento enla lista
+print(lista.index(11))
+
+lista.sort() #Ordena la lista
+print(lista)
+
+lista.sort(reverse=True)  #Otra forma de invertir una lista
+print(lista)
---
+++ arreglos/pila.py
@@ -0,0 +1,14 @@
+#PILA EN PYTHON
+#El último elemento que entra, es el primero que sale
+
+stack = [1, 2, 3, 4, 5]
+stack.append(6)
+stack.append(7)
+
+print(stack)
+
+stack.pop()
+print(stack)
+
+stack.pop()
+print(stack)
